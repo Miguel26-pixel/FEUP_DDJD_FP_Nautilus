54bed842c8982f1beb1ed58fb406c81bf3fa4861
Nuno Alves <nyansword@gmail.com>
2023-06-21 12:22:28 +0100

    add: wind and wave sounds

--- Assets/Scripts/AmbientNoise.cs
+++ Assets/Scripts/AmbientNoise.cs
@@ -7,54 +7,77 @@ using UnityEngine;
 
 public class AmbientNoise : MonoBehaviour
 {
-    private Player player;
-    private Transform playerTransform;
-    private Transform cameraTransform;
+    private Player _player;
+    private Transform _playerTransform;
+    private Transform _cameraTransform;
     
     public EventReference windReference;
     public EventReference waveReference;
     public EventReference shoreReference;
     
-    private EventInstance shoreEvent;
+    private EventInstance _shoreEvent;
+    private EventInstance _waveEvent;
+    private EventInstance _windEvent;
 
     public float shoreLine = 21f;
     public float shoreRange = 5f;
+    public float windRange = 10f;
+    public float waveRange = 5f;
     public float floorDistanceRay = 30f;
     
     public LayerMask floorLayerMask;
     private void Start()
     {
-        player = GameObject.FindWithTag("Player").GetComponent<Player>();
-        playerTransform = player.transform;
+        _player = GameObject.FindWithTag("Player").GetComponent<Player>();
+        _playerTransform = _player.transform;
         if (Camera.main != null)
         {
-            cameraTransform = Camera.main.transform;
+            _cameraTransform = Camera.main.transform;
         }
         
-        InvokeRepeating(nameof(UpdateSounds), 0, 0.5f);
+        InvokeRepeating(nameof(UpdateSounds), 0, 0.1f);
     }
     
     private void UpdateSounds()
     {
-        shoreEvent.getPlaybackState(out var state);
+        _shoreEvent.getPlaybackState(out var state);
+        _windEvent.getPlaybackState(out var windState);
+        _waveEvent.getPlaybackState(out var waveState);
         
         if (state == PLAYBACK_STATE.STOPPED)
         {
-            shoreEvent = RuntimeManager.CreateInstance(shoreReference);
-            shoreEvent.start();
+            _shoreEvent = RuntimeManager.CreateInstance(shoreReference);
+            _shoreEvent.start();
         }
         
-        float floorY = cameraTransform.position.y - floorDistanceRay;
-        if (Physics.Raycast(cameraTransform.position + Vector3.up, Vector3.down, out var hit, 1 + floorDistanceRay,
+        if (windState == PLAYBACK_STATE.STOPPED)
+        {
+            _windEvent = RuntimeManager.CreateInstance(windReference);
+            _windEvent.start();
+        }
+        
+        if (waveState == PLAYBACK_STATE.STOPPED)
+        {
+            _waveEvent = RuntimeManager.CreateInstance(waveReference);
+            _waveEvent.start();
+        }
+        
+        float floorY = _cameraTransform.position.y - floorDistanceRay;
+        if (Physics.Raycast(_cameraTransform.position + Vector3.up, Vector3.down, out var hit, 1 + floorDistanceRay,
                 floorLayerMask))
         {
             floorY = hit.point.y;
         }
 
-        float distanceToShore = Mathf.Abs(floorY - shoreLine);
+        float distanceToShore = floorY - shoreLine;
+        float absDistanceToShore = Mathf.Abs(distanceToShore);
         // 0 when below shore, 1 when on shore, 0 when above shore
-        float shoreFactor = Mathf.Clamp01(1 - distanceToShore / shoreRange);
+        float shoreFactor = Mathf.Clamp01(1 - absDistanceToShore / shoreRange);
+        float waveFactor = Mathf.Clamp01(1 - distanceToShore / waveRange);
+        float windFactor = Mathf.Clamp01(distanceToShore / windRange);
 
-        shoreEvent.setParameterByName("Volume", shoreFactor);
+        _shoreEvent.setParameterByName("Volume", shoreFactor);
+        _windEvent.setParameterByName("Volume", windFactor);
+        _waveEvent.setParameterByName("Volume", waveFactor);
     }
 }
