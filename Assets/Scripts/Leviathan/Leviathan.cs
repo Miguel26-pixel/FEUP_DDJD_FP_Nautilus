5eb5843c561e9f1f52397c3c41dc6e790d94caf3
Miguel Amorim <migamorim2012@gmail.com>
2023-06-22 01:25:19 +0100

    Fix leviathan and spears

--- Assets/Scripts/Leviathan/Leviathan.cs
+++ Assets/Scripts/Leviathan/Leviathan.cs
@@ -6,12 +6,13 @@ using UnityEngine.UI;
 public class Leviathan : MonoBehaviour
 {
     public int health = 100;
+    public int maxHealth = 100;
     public Slider healthBar;
     public Animator animator;
 
     public void Update()
     {
-        healthBar.value = health;
+        healthBar.value = (health/maxHealth)*100;
     }
 
     public void TakeDamage(int damage)
